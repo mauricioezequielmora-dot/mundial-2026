dfe8694763c0d988cdc0bef80363534da4d0144a
mauricioezequielmora-dot <mauricioezequielmora@gmail.com>
2026-07-10 03:02:55 -0300

    Delete titulares.js

--- titulares.js
+++
@@ -1,8 +0,0 @@
-/* Titular de respaldo. Las noticias del panel local tienen prioridad. */
-window.TITULARES = [
-  {
-    fecha: "",
-    texto: "Resultados, posiciones y novedades verificadas durante toda la jornada",
-    fuente: "Central Mundialista"
-  }
-];
